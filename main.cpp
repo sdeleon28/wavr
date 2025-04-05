daaa36dd25cc35e9383c285c282126a42add64c6
Santiago de Leon <sdeleon28@gmail.com>
2025-04-05 18:07:51 -0300

    read wav chunk metadata

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <fstream>
 #include <sstream>
 #include <iostream>
@@ -18,7 +19,31 @@
 
 struct RiffChunkDescriptor {
     char id[5];
+    uint32_t chunk_size;
+    char format[5];
+};
 
+struct FmtSubChunk {
+    char id[5];
+    uint32_t size;
+    uint16_t audio_format;
+    uint16_t num_channels;
+    uint32_t sample_rate;
+    uint32_t byte_rate;
+    uint16_t block_align;
+    uint16_t bits_per_sample;
+};
+
+struct DataSubChunk {
+    char id[5];
+    uint32_t size;
+    std::vector<float> samples;
+};
+
+struct Chunk {
+    RiffChunkDescriptor riff;
+    FmtSubChunk fmt;
+    DataSubChunk data;
 };
 
 int main() {
@@ -44,12 +69,49 @@ int main() {
 
     std::ostringstream out_stream;
 
-    RiffChunkDescriptor riff;
-    riff.id[0] = buffer[0];
-    riff.id[1] = buffer[1];
-    riff.id[2] = buffer[2];
-    riff.id[3] = buffer[3];
-    riff.id[4] = '\0';
+    Chunk chunk;
+
+    int i = 0;
+
+    std::cout << "file size: " << size << std::endl;
+    std::cout << "=== riff ===" << std::endl;
+    chunk.riff.id[0] = buffer[i + 0];
+    chunk.riff.id[1] = buffer[i + 1];
+    chunk.riff.id[2] = buffer[i + 2];
+    chunk.riff.id[3] = buffer[i + 3];
+    chunk.riff.id[4] = '\0';
+    chunk.riff.chunk_size = *reinterpret_cast<uint32_t*>(&buffer[4]);
+    chunk.riff.format[0] = buffer[i + 8];
+    chunk.riff.format[1] = buffer[i + 9];
+    chunk.riff.format[2] = buffer[i + 10];
+    chunk.riff.format[3] = buffer[i + 11];
+    chunk.riff.format[4] = '\0';
+    std::cout << "chunk_size: " << chunk.riff.chunk_size << std::endl;
+    std::cout << "format: " << chunk.riff.format << std::endl;
+
+    std::cout << "=== fmt ====" << std::endl;
+    chunk.fmt.id[0] = buffer[i + 12];
+    chunk.fmt.id[1] = buffer[i + 13];
+    chunk.fmt.id[2] = buffer[i + 14];
+    chunk.fmt.id[3] = buffer[i + 15];
+    chunk.fmt.id[4] = '\0';
+    chunk.fmt.size = *reinterpret_cast<uint32_t*>(&buffer[i + 16]);
+    chunk.fmt.audio_format = *reinterpret_cast<uint16_t*>(&buffer[i + 20]);
+    chunk.fmt.num_channels = *reinterpret_cast<uint16_t*>(&buffer[i + 22]);
+    chunk.fmt.sample_rate = *reinterpret_cast<uint32_t*>(&buffer[i + 24]);
+    chunk.fmt.byte_rate = *reinterpret_cast<uint32_t*>(&buffer[i + 28]);
+    chunk.fmt.block_align = *reinterpret_cast<uint16_t*>(&buffer[i + 32]);
+    chunk.fmt.bits_per_sample = *reinterpret_cast<uint16_t*>(&buffer[i + 34]);
+
+    std::cout << "id: " << chunk.fmt.id << std::endl;
+    std::cout << "size: " << chunk.fmt.size << std::endl;
+    std::cout << "audio_format: " << chunk.fmt.audio_format << std::endl;
+    std::cout << "num_channels: " << chunk.fmt.num_channels << std::endl;
+    std::cout << "sample_rate: " << chunk.fmt.sample_rate << std::endl;
+    std::cout << "byte_rate: " << chunk.fmt.byte_rate << std::endl;
+    std::cout << "block_align: " << chunk.fmt.block_align << std::endl;
+    std::cout << "bits_per_sample: " << chunk.fmt.bits_per_sample << std::endl;
+    std::cout << "=== data ===" << std::endl;
 
     /* std::ofstream out_file("out.txt"); */
     /* if (!out_file.is_open()) { */
